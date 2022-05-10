e5aa937e92687f60d64cfc46e0ed9befb41963c4
DokJS <omarmbengue6919@gmail.com>
2022-05-10 13:30:38 +0000

    delete controller modify

--- Controllers/stuff.js
+++ Controllers/stuff.js
@@ -1,18 +1,18 @@
 const Thing = require('../Models/stuff');
-const jwt = require('jsonwebtoken');
-exports.getAllThing = (req,res,next)=> {
+const auth = require('../Middlewares/auth');
+exports.getAllThing = (req, res, next) => {
     Thing.find()
-    .then( allStuff => res.status(201).json({catalogue: allStuff}))
-    .catch( error => res.status(400).json({errorMessage: error}))
+        .then(allStuff => res.status(201).json({ catalogue: allStuff }))
+        .catch(error => res.status(400).json({ errorMessage: error }))
 };
 
-exports.getOneThing = (req,res,next) => {
-    Thing.findOne({_id:req.params.id})
-    .then( thing => res.status(201).json({article: thing}))
-    .catch( error => res.status(400).json({errorMessage: error}))
+exports.getOneThing = (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+        .then(thing => res.status(201).json({ article: thing }))
+        .catch(error => res.status(400).json({ errorMessage: error }))
 }
 
-exports.modifyOneThing = (req,res,next)=> {
+exports.modifyOneThing = (req, res, next) => {
     // this define the thing which will replace the current thing
     const newThing = new Thing({
         _id: req.params.id,
@@ -22,42 +22,39 @@ exports.modifyOneThing = (req,res,next)=> {
         userId: req.body.userId,
         price: req.body.userId
     })
-    Thing.updateOne({_id:req.params.id},newThing)
-    .then(()=> res.status(200).json({message: ' article modified'}))
-    .catch(erro => res.status(400).json({message: 'modification failed'}))
+    Thing.updateOne({ _id: req.params.id }, newThing)
+        .then(() => res.status(200).json({ message: ' article modified' }))
+        .catch(erro => res.status(400).json({ message: 'modification failed' }))
 }
 
-    
- // find the thing that user wanna delete
- // verify if user have authorization for delete them
- // if true delete the thing
- // else send errorMessage 
-exports.deleteOneThing = (req,res,next)=> {
-    Thing.findOne({_id: req.params.id})
-    .then( thing => {
-         const requestHeadersData = req.headers.authorization.split(' ');
-         const token =  requestHeadersData[1];
-         const requestUserId = jwt.verify(token, 'RANDOM_TOKEN_SECRET').userId;
-         if(requestUserId === thing.userId){
-             Thing.deleteOne({_id:req.params.id})
-             .then(()=> res.status(200).json({message: 'article deleted !'}))
-             .catch(error => res.status(400).json({error}));
-         }else {
-             return res.status(400).json({message: ' unauthorized request '})
-         }
-    })
-    .catch()
+
+// find the thing that user wanna delete
+// verify if user have authorization for delete them
+// if true delete the thing
+// else send errorMessage 
+exports.deleteOneThing = (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+        .then(thing => {
+            if (auth.userId === thing.userId) {
+                Thing.deleteOne({ _id: req.params.id })
+                    .then(() => res.status(200).json({ message: 'article deleted !' }))
+                    .catch(error => res.status(400).json({ error }));
+            } else {
+                return res.status(400).json({ message: ' unauthorized request !' })
+            }
+        })
+        .catch( error => res.status(400).json({error}))
 }
 
-exports.addOneThing = (req,res,next)=> {
+exports.addOneThing = (req, res, next) => {
     const newThing = new Thing({
-        title:req.body.title,
+        title: req.body.title,
         description: req.body.description,
         imageUrl: req.body.imageUrl,
         userId: req.body.userId,
-        price:req.body.price
+        price: req.body.price
     })
     newThing.save()
-    .then( newThing => res.status(200).json({message: 'article added'}))
-    .catch( error => res.status(400).json({message: error}))
+        .then(newThing => res.status(200).json({ message: 'article added' }))
+        .catch(error => res.status(400).json({ message: error }))
 }

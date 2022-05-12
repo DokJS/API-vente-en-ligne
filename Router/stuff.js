ccd840acbd7990e727c8e6ad502391b01a146158
DokJS <omarmbengue6919@gmail.com>
2022-05-12 14:51:14 +0000

    incoming files management

--- Router/stuff.js
+++ Router/stuff.js
@@ -2,14 +2,15 @@ const express = require('express');
 // initializes the router
 const router = express.Router();
 const stuffControllers = require('../Controllers/stuff');
-const auth = require('../Middlewares/auth')
+const auth = require('../Middlewares/auth');
+const multer = require('../Middlewares/multer-config');
 
 // Routes
 router.get('/',auth,stuffControllers.getAllThing);
 router.get('/:id',auth,stuffControllers.getOneThing);
-router.put('/:id',auth,stuffControllers.modifyOneThing);
+router.put('/:id',auth,multer,stuffControllers.modifyOneThing);
 router.delete('/:id',auth,stuffControllers.deleteOneThing);
-router.post('/',auth,stuffControllers.addOneThing)
+router.post('/',auth,multer,stuffControllers.addOneThing)
 
 
 module.exports = router;
